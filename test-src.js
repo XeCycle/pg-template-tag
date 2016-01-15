c7c9261bdeee10f4da973e344e3bd5f393f9dcfe
Carl Lei <xecycle@gmail.com>
2016-01-15 17:19:11 +0800

    Reorganize tests

--- test-src.js
+++ test-src.js
@@ -2,21 +2,26 @@ import {default as SQL, SQL_dedup as _SQL, join} from "./";
 import assert from "assert";
 
 describe("pg-template-tag", function() {
-  it("collects values and write placeholders", function() {
-    var literal = SQL`foo ${123} ${"abc"} bar ${null}`;
-    assert.equal(literal.text, "foo $1 $2 bar $3");
-    assert.deepEqual(literal.values, [123, "abc", null]);
-  });
 
-  it("accepts nested literals", function() {
-    var literal = SQL`foo ${SQL`bar ${1} ${2} ${SQL`baz`}`} ${SQL`foobar ${3}`} ${4}`;
-    assert.equal(literal.text, "foo bar $1 $2 baz foobar $3 $4");
-    assert.deepEqual(literal.values, [1, 2, 3, 4]);
-  });
+  describe("default export (SQL)", function() {
 
-  it("does not interpolate arrays into values array", function() {
-    var literal = SQL`${[1, 2, 3]}`;
-    assert.deepEqual(literal.values, [[1, 2, 3]]);
+    it("collects values and write placeholders", function() {
+      var literal = SQL`foo ${123} ${"abc"} bar ${null}`;
+      assert.equal(literal.text, "foo $1 $2 bar $3");
+      assert.deepEqual(literal.values, [123, "abc", null]);
+    });
+
+    it("accepts nested literals", function() {
+      var literal = SQL`foo ${SQL`bar ${1} ${2} ${SQL`baz`}`} ${SQL`foobar ${3}`} ${4}`;
+      assert.equal(literal.text, "foo bar $1 $2 baz foobar $3 $4");
+      assert.deepEqual(literal.values, [1, 2, 3, 4]);
+    });
+
+    it("does not interpolate arrays into values array", function() {
+      var literal = SQL`${[1, 2, 3]}`;
+      assert.deepEqual(literal.values, [[1, 2, 3]]);
+    });
+
   });
 
   describe("SQL_dedup", function() {
@@ -41,20 +46,25 @@ describe("pg-template-tag", function() {
 
   });
 
-  it("joins SQL instances", function () {
-    var literal = join([SQL`foo`, SQL`bar ${123}`, SQL`${456}`], " sep ");
-    assert.equal(literal.text, "foo sep bar $1 sep $2");
-    assert.deepEqual(literal.values, [123, 456]);
+  describe("join", function() {
+
+    it("joins SQL instances", function () {
+      var literal = join([SQL`foo`, SQL`bar ${123}`, SQL`${456}`], " sep ");
+      assert.equal(literal.text, "foo sep bar $1 sep $2");
+      assert.deepEqual(literal.values, [123, 456]);
+    });
+
+    it("joins with default comma", function () {
+      var literal = join([SQL`foo`, SQL`bar`]);
+      assert.equal(literal.text, "foo,bar");
+    });
+
+    it("joins other types", function () {
+      var literal = join([1, 'hello', [1, 2, 3]]);
+      assert.equal(literal.text, "$1,$2,$3");
+      assert.deepEqual(literal.values, [1, 'hello', [1, 2, 3]]);
+    });
+
   });
 
-  it("joins with default comma", function () {
-    var literal = join([SQL`foo`, SQL`bar`]);
-    assert.equal(literal.text, "foo,bar");
-  });
-
-  it("joins other types", function () {
-    var literal = join([1, 'hello', [1, 2, 3]]);
-    assert.equal(literal.text, "$1,$2,$3");
-    assert.deepEqual(literal.values, [1, 'hello', [1, 2, 3]]);
-  });
 });
