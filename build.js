b1b662d8ef58e0103a0e5519bc6f575548547288
Paul Draper <paulddraper@gmail.com>
2018-09-02 15:54:42 -0600

    Fix build.js for new Node.js

--- build.js
+++ build.js
@@ -7,6 +7,6 @@ babel.transformFile("test-src.js", writeTo("test.js"));
 function writeTo(name) {
   return function writer(err, content) {
     if (err) throw err;
-    fs.writeFile(name, content.code);
+    fs.writeFile(name, content.code, function() {});
   };
 }
